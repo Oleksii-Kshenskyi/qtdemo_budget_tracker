3dd957146ee0841d773bc724f839ff0d9ae3791d
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2024-04-02 17:28:37 +0300

    Qt Creator generates a lot of trash that does not work, a dummy TableView finally compiles.

--- DemoBudgetTracker/main.qml
+++ DemoBudgetTracker/main.qml
@@ -1,8 +1,21 @@
-/* This file is generated and only relevant for integrating the project into a Qt 6 and cmake based
-C++ project. */
-
 import QtQuick
 import content
 
 App {
+    TableView {
+        anchors.fill: parent
+        columnSpacing: 1
+        rowSpacing: 1
+        clip: true
+
+        model: TableModel {}
+
+        delegate: Rectangle {
+            implicitWidth: 100
+            implicitHeight: 50
+            Text {
+                text: display
+            }
+        }
+    }
 }
---
+++ DemoBudgetTracker/src/tablemodel.h
@@ -0,0 +1,24 @@
+#ifndef TABLEVIEW_H
+#define TABLEVIEW_H
+
+#include <QObject>
+#include <QQmlEngine>
+
+#include <qqml.h>
+#include <QAbstractTableModel>
+
+class TableModel : public QAbstractTableModel
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_ADDED_IN_VERSION(1, 1)
+
+public:
+    int rowCount(const QModelIndex & = QModelIndex()) const override;
+    int columnCount(const QModelIndex & = QModelIndex()) const override;
+
+    QVariant data(const QModelIndex &index, int role) const override;
+    QHash<int, QByteArray> roleNames() const override;
+};
+
+#endif // TABLEVIEW_H
